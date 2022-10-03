46ed917fe5e6abeeed7b96b6d581011a46e0c2a2
Kohei Mito <53444364+R-M-kohei@users.noreply.github.com>
2022-10-03 15:40:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if uploaded_data_csv:
         if (len(Y_columns)>0) & (len(X_columns)>0):
             st.write("5.Get the next condition")
             perform = st.button("Perform")
-            num_X_for_predictions = int(3e6)
+            num_X_for_predictions = int(e6)
             if perform:
                 st.text("Modeling of Gaussian Processing...")
                 # settings
